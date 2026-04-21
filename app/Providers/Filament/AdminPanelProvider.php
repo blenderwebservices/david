6a3432757b8c5aa806e4bea960dbb6b84aba68c2
FRancisco <blender.webservices@gmail.com>
2026-04-21 00:18:45 -0600

    chore: update build assets and refresh Filament admin panel manifest

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,6 +26,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->login()
             ->homeUrl('/')
             ->navigationItems([
                 NavigationItem::make('Ver Sitio')
